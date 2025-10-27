2783e7183bb5136437eafd7b029ec7f299f6666e
Alban <alban.suard@gmail.com>
2025-10-27 09:29:54 +0400

    Initialize Nuxt 4 project with essential configurations and dependencies

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/image',
+    '@nuxtjs/tailwindcss',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxtjs/seo'
+  ]
+})
